184a2aec130ac486a1c7f52a6fd94746edd72143
marcin <kto tam@>
2022-03-14 10:37:50 +0100

    asdad

--- components/PlaceholderPage.js
+++ components/PlaceholderPage.js
@@ -2,10 +2,17 @@ import styles from '../styles/FallbackPage.module.css'
 const PlaceholderPage = () => {
     return ( 
         <div className={styles.page} >
-<h1></h1>
-<div>
+<h1>Loading...</h1>
+<div className="wait">
 
 </div>
+<style>{`
+.wait{
+    width:80%;
+    height:60%;
+    box-shadow:3px 3px 10px 10px black
+}
+`}</style>
     </div> 
      );
 }
